e4bd0278f0d9010fdaf97bbdeb337380f0cf6be9
Ryan Maas <rdmjobs@live.com>
2021-05-28 17:36:53 -0500

    Added client side post functionality.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,91 +1,148 @@
+//#region ⬇⬇ Document setup & event handlers below:
+// ⬇ Document setup below:
 console.log('JavaScript loaded!');
-$(document).ready(function(){
+$(document).ready(function () {
   console.log('jQuery loaded!');
   // refreshDom();
-  addClickHandlers();
+  addEventHandlers();
 }); // End document ready. 
 
-function addClickHandlers() {
-  $( '#taskInputArea' ).on( 'click', $('#submitButton'), clickedSubmit )
+// ⬇ Event handlers below:
+function addEventHandlers() {
+  $('#taskInputArea').on('click', $('#submitButton'), clickedSubmit)
+} // End addEventHandlers(). 
+//#endregion ⬆⬆ Document setup & event handlers above. 
+
+
+
+
+// refreshBooks will get all books from the server and render to page
+function refreshBooks() {
+  $.ajax({
+    type: 'GET',
+    url: '/books'
+  }).then(function (response) {
+    console.log(response);
+    renderBooks(response);
+  }).catch(function (error) {
+    console.log('error in GET', error);
+  });
 }
 
+
+// Displays an array of books to the DOM
+function renderBooks(books) {
+  $('#bookShelf').empty();
+
+  for (let i = 0; i < books.length; i += 1) {
+    let book = books[i];
+    // For each book, append a new row to our table
+    $('#bookShelf').append(`
+      <tr>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isRead}</td>
+        <td><button class="readButtons" data-id="${book.id}">Mark As Read</button></td>
+        <td><button class="deleteButtons" data-id="${book.id}">Delete Book</button></td>
+      </tr>
+    `);
+  }
+}
+
+
+
+// ⬇ clickedSubmit functionality below:
 function clickedSubmit() {
-  console.log( 'In clickedSubmit' );
+  console.log('In clickedSubmit');
+  // ⬇ Declaring empty object variable to hold the task input:
   let task = {};
   task.name = $('#taskInput').val();
-  $.ajax
-
-} // End clickedSubmit function. 
-
-function handleSubmit() {
-  console.log('Submit button clicked.');
-  let book = {};
-  book.author = $('#author').val();
-  book.title = $('#title').val();
-  addBook(book);
-} // End handleSubmit()
-
-// adds a book to the database
-function addBook(bookToAdd) {
+  // ⬇ Sending that input to the server /tasks POST route:
   $.ajax({
-    type: 'POST',
-    url: '/books',
-    data: bookToAdd,
-    }) // End .ajax
-      .then(function(response) {
-        console.log('Response from server.', response);
-        refreshBooks();
+    method: 'POST',
+    url: '/tasks',
+    data: task,
+  }) // End .ajax
+    .then(response => {
+      console.log('In clickedSubmit .then, response:', response);
+      refreshDom();
     }) // End .then
-      .catch(function(error) {
-        console.log('Error in POST', error)
-        alert('Unable to add book at this time. Please try again later.');
+    .catch(error => {
+      console.log('In clickedSubmit .catch, error:', error);
+      alert('Unable to add task at this time, please try again later.')
     }); // End .catch
-} // End addBook()
+
+} // End clickedSubmit(). 
+
+// function handleSubmit() {
+//   console.log('Submit button clicked.');
+//   let book = {};
+//   book.author = $('#author').val();
+//   book.title = $('#title').val();
+//   addBook(book);
+// } // End handleSubmit()
+
+// // adds a book to the database
+// function addBook(bookToAdd) {
+//   $.ajax({
+//     type: 'POST',
+//     url: '/books',
+//     data: bookToAdd,
+//   }) // End .ajax
+//     .then(function (response) {
+//       console.log('Response from server.', response);
+//       refreshBooks();
+//     }) // End .then
+//     .catch(function (error) {
+//       console.log('Error in POST', error)
+//       alert('Unable to add book at this time. Please try again later.');
+//     }); // End .catch
+// } // End addBook()
 
 // Function to handle the click event and pass the book id to the deleteBook function:
 function readHandler() {
-  console.log( 'In readHandler' );
-  markedRead( $(this).data( "id" ) );
+  console.log('In readHandler');
+  markedRead($(this).data("id"));
 } // End readHandler()
 
 function markedRead(bookId) {
-  console.log( 'In markedRead' );
+  console.log('In markedRead');
   $.ajax({
-    method: 'PUT', 
+    method: 'PUT',
     url: `books/${bookId}`,
     data: {
       isRead: true // Will be on: req.body.isRead
     }
   }) // End .ajax
-    .then( response => {
-      console.log( 'In PUT /books/id. Response:', response );
+    .then(response => {
+      console.log('In PUT /books/id. Response:', response);
       refreshBooks();
     }) // End .then
-    .catch( error => {
-      console.log( 'In PUT /books/id. Error:', error );
-      alert( `There was an error with markedRead:`, error )
+    .catch(error => {
+      console.log('In PUT /books/id. Error:', error);
+      alert(`There was an error with markedRead:`, error)
     }); // End .catch
 } // End markRead()
 
 // Function to handle the click event and pass the book id to the deleteBook function:
 function deleteHandler() {
-  console.log( 'In deleteHandler' );
-  deleteBook( $(this).data( "id" ) );
+  console.log('In deleteHandler');
+  deleteBook($(this).data("id"));
 } // End deleteHandler()
 
 function deleteBook(bookId) {
-  console.log( 'In deleteBook' );
+  console.log('In deleteBook');
   $.ajax({
     method: 'DELETE',
     url: `/books/${bookId}`,
   }) // End .ajax
-    .then( response => {
-      console.log( 'In DELETE /books/id. Response:', response ); 
+    .then(response => {
+      console.log('In DELETE /books/id. Response:', response);
       refreshBooks();
     }) // End .then
-    .catch( error => {
-      console.log( 'In DELETE /books/id. Error:', error );
-      alert( `There was a problem deleting that book, please try again:`, error );
+    .catch(error => {
+      console.log('In DELETE /books/id. Error:', error);
+      alert(`There was a problem deleting that book, please try again:`, error);
     }); // End .catch
 } // End deleteBook()
 
@@ -96,10 +153,10 @@ function refreshBooks() {
   $.ajax({
     type: 'GET',
     url: '/books'
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(response);
     renderBooks(response);
-  }).catch(function(error){
+  }).catch(function (error) {
     console.log('error in GET', error);
   });
 }
@@ -109,7 +166,7 @@ function refreshBooks() {
 function renderBooks(books) {
   $('#bookShelf').empty();
 
-  for(let i = 0; i < books.length; i += 1) {
+  for (let i = 0; i < books.length; i += 1) {
     let book = books[i];
     // For each book, append a new row to our table
     $('#bookShelf').append(`
